888f68df14b0fefbad005ab4024caceb458022f0
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-05-13 19:14:57 +0200

    init: project

---
+++ src/main/java/org/delivery/DeliveryAndParcelManagementApplication.java
@@ -0,0 +1,12 @@
+package org.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAndParcelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAndParcelManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/org/delivery/DeliveryAndParcelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAndParcelManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
